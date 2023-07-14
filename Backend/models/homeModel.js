0da16046d9a069e61ec4594dba73d480bb7533a7
Malika Auma Omwayi <mamajedeia@gmail.com>
2023-07-14 17:34:55 +0300

    put back commented out section of homeModel.js

--- Backend/models/homeModel.js
+++ Backend/models/homeModel.js
@@ -1,7 +1,34 @@
 const mongoose = require("mongoose");
 
 const HomeSchema = new mongoose.Schema({
-  
+  /*    name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    photos: {
+        type: [String],    //an array of string because we want many photos 
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+    */
   title: { type: String, required: true },
   text: { type: String, required: true },
   image: { type: String, required: true },
