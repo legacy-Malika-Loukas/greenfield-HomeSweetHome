e7c430ad8c9f4be9dbd47b52b1acc9403b371be9
Malika Auma Omwayi <mamajedeia@gmail.com>
2023-07-16 19:52:46 +0300

    removed get by id from routers

--- Backend/router/homeRouter.js
+++ Backend/router/homeRouter.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const homeController = require("../controllers/homeController");
 
 router.get("/", homeController.getAllHomes);
-router.get("/:id", homeController.getOneHome);
 router.post("/", homeController.postHome);
 router.delete("/:id", homeController.deleteHome);
 router.put("/:id", homeController.updateHome);
